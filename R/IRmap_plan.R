f2ed815d5092715f270e184547485b34e6474d3a
Mathis Loïc Messager <messamat@uw.edu>
2020-04-09 15:36:38 -0400

    Transfer workflow to Drake all the way to before tuning

--- R/IRmap_plan.R
+++ R/IRmap_plan.R
@@ -1,8 +1,13 @@
 plan <- drake_plan(
-  file_structure = def_filestructure(),
-  gaugep = read_gaugep(file_structure, dist=200),
-  gauged_filenames = read_gauged_paths(file_structure, gaugep),
-  gaugestats = target(durfreq_indiv(gauged_filenames, maxgap=20, monthsel=NULL), 
-                       dynamic = map(gauged_filenames))
+  filestructure = def_filestructure(),
+  gaugep = read_gaugep(in_filestructure=filestructure, dist=200),
+  gauged_filenames = read_gauged_paths(filestructure, gaugep),
+  gaugestats = future_map(gauged_filenames, #To map
+                          comp_durfreq, #Function to run on each file name
+                          maxgap=20, monthsel=NULL, #Other arguments in function
+                          .progress = TRUE),
+  gaugestats_format = format_gaugestats(gaugestats, gaugep),
+  predvars = selectformat_predvars(filestructure, gaugestats_format)
 )
+
   
