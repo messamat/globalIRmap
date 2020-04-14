9c63a94b332507747bd998b323b5467178e0c2b9
messamat <mathis.messager@mail.mcgill.ca>
2020-04-14 14:46:47 -0400

    Clean up

--- IntermittentAnalysis_MasterScript_reproduced.R
+++ IntermittentAnalysis_MasterScript_reproduced.R
@@ -1,39 +1,4 @@
 
-
-
-
-
-
-
-
-
-##################################################################################################################################################
-############################ MORE ALREADY SPED UP/TRANSCRIBED ####################################################################################
-#Set default ranger learner with explicit parameter set
-# rangerlrn <- mlr3::lrn('classif.ranger', id = 'ranger', 
-#                        num.trees = 500,
-#                        #mtry = , #Default is square root of number of variables
-#                        min.node.size = 1, #Default is 1 for classification, 5 for regression and 10 for probability
-#                        replace = FALSE, #in ranger package, default replace is True but Boulesteix et al. 2012 show that less biased
-#                        sample.fraction = 0.632, #Default for sampling without replacement
-#                        split.select.weights = ,
-#                        always.split.variables= ,
-#                        respect.unordered.factors = 'ignore',
-#                        importance='permutation',
-#                        write.forest=TRUE, 
-#                        scale.permutation.importance = FALSE,
-#                        num.threads = bigstatsr::nb_cores(),
-#                        save.memory = FALSE,
-#                        verbose = TRUE,
-#                        splitrule = "gini",
-#                        num.random.splits = 1L,
-#                        keep.inbag = FALSE,
-#                        predict_type = 'prob',
-#                        max.depth = NULL #Unlimited depth default
-# )
-
-
-
 #---- Get mDur and mFreq in winter and non-winter periods to assess whether intermittency is due to freezing ----
 
 #Compute 3-month period of minimum temperature
@@ -50,26 +15,13 @@ mintemplist<- dcast(
 names(mintemplist) <- reaches_with_points$GRDC_NO
 
 #Compute mdur and mfreq for winter months
-in_gaugestats_winter <- durfreq_parallel(pathlist=fileNames, maxgap=20, monthsel_list=mintemplist, reverse=FALSE)
+gaugestats_winter <- durfreq_parallel(pathlist=fileNames, maxgap=20, monthsel_list=mintemplist, reverse=FALSE)
 
 #Compute mdur and mfreq for non-winter months
-in_gaugestats_nowinter <- durfreq_parallel(pathlist=fileNames, maxgap=20, monthsel_list=mintemplist, reverse=TRUE)
+gaugestats_nowinter <- durfreq_parallel(pathlist=fileNames, maxgap=20, monthsel_list=mintemplist, reverse=TRUE)
 
 #---- Check month of intermittency ----
-monthinter_melt <- melt(in_gaugestats[, c('GRDC_NO', "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"), with=F],
+monthinter_melt <- melt(gaugestats[, c('GRDC_NO', "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"), with=F],
                         id.var='GRDC_NO')
 ggplot(monthinter_melt, aes(x=variable, y=value, fill=GRDC_NO)) +
   geom_area(stat='identity')
-
-#Base ranger training
-#To adapt
-OOBfulldat <- OOBCurve(mod=mod_basic, measures = list(mmce, auc, brier), task = intermittent.task, data = in_gaugestats_format[!is.na(cly_pc_cav),]) %>%
-  setDT %>%
-  .[, numtrees := .I]
-
-#Plot it
-ggplot(melt(OOBfulldat, id.vars = 'numtrees'), aes(x=numtrees, y=value, color=variable)) + 
-  geom_point(size=1, alpha=0.5) + 
-  scale_y_continuous(limits=c(0,1), expand=c(0,0), breaks=seq(0,1,0.1)) + 
-  scale_x_continuous(expand=c(0,0), breaks = seq(0,1000,100)) + 
-  theme_bw()
