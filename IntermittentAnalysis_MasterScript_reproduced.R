37e226fe9d40d0efb5cde463a6101f5635971b4a
Mathis Loïc Messager <mathis.messager@mail.mcgill.ca>
2020-04-15 15:15:15 -0400

    Test adaptation of diagnostic functions to using outputs from GraphLearner

--- IntermittentAnalysis_MasterScript_reproduced.R
+++ IntermittentAnalysis_MasterScript_reproduced.R
@@ -5,13 +5,13 @@ source('R/IRmapping_plan.R')
 
 in_gaugestats= readd(gaugestats_format)
 in_predvars = readd(predvars)
+in_rftuned = readd(rftuned)
 insamp_nfolds = 2
 insamp_neval = 20
 insamp_nbatch = parallel::detectCores(logical=FALSE)
 outsamp_nrep = 1
 outsamp_nfolds = 5
 
-#Create task
 datsel <- in_gaugestats[!is.na(cly_pc_cav), 
                         c('intermittent',in_predvars$varcode),
                         with=F]
@@ -27,31 +27,11 @@ lrn_ranger <- mlr3::lrn('classif.ranger',
                         importance = "permutation")
 #print(lrn_ranger$param_set)
 
-#Create mlr3 pipe operator to oversample minority class based on major/minor ratio
-#https://mlr3gallery.mlr-org.com/mlr3-imbalanced/
-#https://mlr3pipelines.mlr-org.com/reference/mlr_pipeops_classbalancing.html
-#Sampling happens only during training phase.
-po_over <- po("classbalancing", id = "oversample", adjust = "minor", 
-              reference = "minor", shuffle = TRUE, 
-              ratio = get_oversamp_ratio(task_inter)$ratio)
-#table(po_over$train(list(task_inter))$output$truth()) #Make sure that oversampling worked
-
-#Create a graph learner so that oversampling happens systematically upstream of all training
-lrn_ranger_over <- GraphLearner$new(po_over %>>% lrn_ranger)
-
-#Define parameter ranges to tune on
-regex_tuneset <- function(in_lrn) {
-  prmset <- names(in_lrn$param_set$tags)
-  
-  tune_ranger <- ParamSet$new(list(
-    ParamInt$new(prmset[grep(".*mtry", prmset)], 
-                 lower = 1, upper = 11),
-    ParamInt$new(prmset[grep(".*min.node.size", prmset)], 
-                 lower = 1, upper = 10),
-    ParamDbl$new(prmset[grep(".*sample.fraction", prmset)], 
-                 lower = 0.1, upper = 0.95)
-  ))
-}
+tune_ranger <- ParamSet$new(list(
+  ParamInt$new("mtry", lower = 1, upper = 11),
+  ParamInt$new("min.node.size", lower = 1, upper = 10),
+  ParamDbl$new("sample.fraction", lower = 0.1, upper = 0.95)
+))
 
 #Define inner resampling strategy
 rcv_ranger = rsmp("cv", folds=insamp_nfolds) #5-fold aspatial CV repeated 10 times
@@ -60,51 +40,52 @@ measure_ranger = msr("classif.bacc") #use balanced accuracy to account for imbal
 #Define termination rule 
 evals20 = term("evals", n_evals = insamp_neval) #termine tuning after 20 rounds
 
-#Define hyperparameter tuner wrapper for inner sampling
-learns = list(
-  AutoTuner$new(learner= lrn_ranger,
-                resampling = rcv_ranger, 
-                measures = measure_ranger,
-                tune_ps = regex_tuneset(lrn_ranger), 
-                terminator = evals20,
-                tuner =  tnr("random_search", 
-                             batch_size = insamp_nbatch)), #batch_size determines level of parallelism
+at_ranger <- AutoTuner$new(learner= lrn_ranger,
+                           resampling = rcv_ranger, 
+                           measures = measure_ranger,
+                           tune_ps = tune_ranger, 
+                           terminator = evals20,
+                           tuner =  tnr("random_search", 
+                                        batch_size = insamp_nbatch))
+
+nestedresamp_ranger <- mlr3::resample(task = task_inter, 
+                                      learner = at_ranger, 
+                                      resampling = rsmp("repeated_cv", 
+                                                        repeats = 1, 
+                                                        folds = 2),
+                                      store_models=TRUE)
+
+loadd(rftuned)
+in_mod <- rftuned$rf_outer$learners[[1]] 
+in_mod <- nestedresamp_ranger$learners[[1]]
+
+
+#in_mod <- nestedresamp_ranger$learners[[1]] 
+
+if (inherits(in_mod$learner, "GraphLearner")) {
+  in_fit <- in_mod$learner$model$classif.ranger$model
+} else {
+  in_fit <- in_mod$learner$model 
+}
+class(in_fit)
+in_fit
+
+foldperf <- extract_impperf_nestedrf(in_mod, imp=F, perf=T)
+
+# selcols <- in_vimp_plot$data %>% #Can use that if extracting from tunredrf is expensive
+#   setorder(-imp_wmean) %>%
+#   .[colnums, variable]
+ngrid <- c(10,10)
+selcols <- c('run_mm_cyr', 'dis_m3_pmn')
+datdf <- as.data.frame(task_inter$data())
+rftuned$rf_outer$task$data()
+nestedresamp_ranger$task$data()
+
+pdout <- edarf::partial_dependence(in_fit, vars = selcols, n = ngrid,
+                                   interaction = TRUE, data = datdf) #Warning: does not work with data_table
   
-  AutoTuner$new(learner= lrn_ranger_over,
-                resampling = rcv_ranger, 
-                measures = measure_ranger,
-                tune_ps = regex_tuneset(lrn_ranger_over), 
-                terminator = evals20,
-                tuner =  tnr("random_search", 
-                             batch_size = insamp_nbatch)) 
-)
-names(learns) <-mlr3misc::map(learns, "id")
-
-#Perform outer resampling, keeping models for diagnostics later
-outer_resampling = rsmp("repeated_cv", 
-                        repeats = outsamp_nrep, 
-                        folds = outsamp_nfolds)
-
-nestedresamp_bmrdesign <- benchmark_grid(tasks = task_inter, 
-                                         learners = learns, 
-                                         resamplings = outer_resampling)
-
-nestedresamp_bmrout <- benchmark(nestedresamp_bmrdesign,
-                                store_models = TRUE)
-
-print(nestedresamp_bmrout$aggregate(measure_ranger))
-mlr3viz::autoplot(nestedresamp_bmrout, measure = measure_ranger)
-
-#Train learners
-learns$classif.ranger.tuned$train(task_inter) 
-learns$oversample.classif.ranger.tuned$train(task_inter)
-
-#Return outer sampling object for selected model
-nestedresamp_bmrout$resample_result(
-  which(names(learns)=="oversample.classif.ranger.tuned"))
-
-rfresamp <- rftuned$rf_outer
-
+  
+  
 
 rftuned$rf_outer$aggregate(msr('time_both'))
 
