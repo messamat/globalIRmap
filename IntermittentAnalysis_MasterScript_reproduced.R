29f02a9f6c7c0c0f0f9ea0b5085fbc66e925aed3
Mathis Loïc Messager <mathis.messager@mail.mcgill.ca>
2020-04-14 20:39:43 -0400

    Implement random oversampling and compare it to model training on imbalanced dataset

--- IntermittentAnalysis_MasterScript_reproduced.R
+++ IntermittentAnalysis_MasterScript_reproduced.R
@@ -3,21 +3,110 @@ source('R/IRmapping_packages.R')
 source('R/IRmapping_functions.R')
 source('R/IRmap_plan.R')
 
-loadd(rftuned)
+in_gaugestats= readd(gaugestats_format)
+in_predvars = readd(predvars)
+insamp_nfolds = 2
+insamp_neval = 20
+insamp_nbatch = parallel::detectCores(logical=FALSE)
+outsamp_nrep = 1
+outsamp_nfolds = 5
 
-#Get number of cases for each class
-get_oversamp_ratio <- function(in_task) {
-  return(
-    in_task$data()[, .N,
-                   by=get(rftuned$task_inter$target_names)] %>%
-      setorder(N) %>%
-      .[, list(minoclass=get[1], ratio=N[2]/N[1])]
-  )
+#Create task
+datsel <- in_gaugestats[!is.na(cly_pc_cav), 
+                        c('intermittent',in_predvars$varcode),
+                        with=F]
+task_inter <- mlr3::TaskClassif$new(id ='inter_basic',
+                                    backend = datsel,
+                                    target = "intermittent")
+
+#Create learner
+lrn_ranger <- mlr3::lrn('classif.ranger', 
+                        num.trees=500, 
+                        replace=F, 
+                        predict_type="prob", 
+                        importance = "permutation")
+#print(lrn_ranger$param_set)
+
+#Create mlr3 pipe operator to oversample minority class based on major/minor ratio
+#https://mlr3gallery.mlr-org.com/mlr3-imbalanced/
+#https://mlr3pipelines.mlr-org.com/reference/mlr_pipeops_classbalancing.html
+#Sampling happens only during training phase.
+po_over <- po("classbalancing", id = "oversample", adjust = "minor", 
+              reference = "minor", shuffle = TRUE, 
+              ratio = get_oversamp_ratio(rftuned$task_inter)$ratio)
+#table(po_over$train(list(rftuned$task_inter))$output$truth()) #Make sure that oversampling worked
+
+#Create a graph learner so that oversampling happens systematically upstream of all training
+lrn_ranger_over <- GraphLearner$new(po_over %>>% lrn_ranger)
+
+#Define parameter ranges to tune on
+regex_tuneset <- function(in_lrn) {
+  prmset <- names(in_lrn$param_set$tags)
+  
+  tune_ranger <- ParamSet$new(list(
+    ParamInt$new(prmset[grep(".*mtry", prmset)], 
+                 lower = 1, upper = 11),
+    ParamInt$new(prmset[grep(".*min.node.size", prmset)], 
+                 lower = 1, upper = 10),
+    ParamDbl$new(prmset[grep(".*sample.fraction", prmset)], 
+                 lower = 0.1, upper = 0.95)
+  ))
 }
 
+#Define inner resampling strategy
+rcv_ranger = rsmp("cv", folds=insamp_nfolds) #5-fold aspatial CV repeated 10 times
+#Define performance measure
+measure_ranger = msr("classif.bacc") #use balanced accuracy to account for imbalanced dataset
+#Define termination rule 
+evals20 = term("evals", n_evals = insamp_neval) #termine tuning after 20 rounds
+
+#Define hyperparameter tuner wrapper for inner sampling
+learns = list(
+  AutoTuner$new(learner= lrn_ranger,
+                resampling = rcv_ranger, 
+                measures = measure_ranger,
+                tune_ps = regex_tuneset(lrn_ranger), 
+                terminator = evals20,
+                tuner =  tnr("random_search", 
+                             batch_size = insamp_nbatch)), #batch_size determines level of parallelism
   
+  AutoTuner$new(learner= lrn_ranger_over,
+                resampling = rcv_ranger, 
+                measures = measure_ranger,
+                tune_ps = regex_tuneset(lrn_ranger_over), 
+                terminator = evals20,
+                tuner =  tnr("random_search", 
+                             batch_size = insamp_nbatch)) 
+)
+names(learns) <-mlr3misc::map(learns, "id")
 
+#Perform outer resampling, keeping models for diagnostics later
+outer_resampling = rsmp("repeated_cv", 
+                        repeats = outsamp_nrep, 
+                        folds = outsamp_nfolds)
 
+nestedresamp_bmrdesign <- benchmark_grid(tasks = task_inter, 
+                                         learners = learns, 
+                                         resamplings = outer_resampling)
+
+nestedresamp_bmrout <- benchmark(nestedresamp_bmrdesign,
+                                store_models = TRUE)
+
+print(nestedresamp_bmrout$aggregate(measure_ranger))
+mlr3viz::autoplot(nestedresamp_bmrout, measure = measure_ranger)
+
+#Train learners
+learns$classif.ranger.tuned$train(task_inter) 
+learns$oversample.classif.ranger.tuned$train(task_inter)
+
+#Check confusion matrix
+pred_basic<- learns$classif.ranger.tuned$predict(task_inter)
+pred_basic$confusion
+pred_basic$score(measure_ranger)
+
+pred_basicover <- learns$oversample.classif.ranger.tuned$predict(task_inter)
+pred_basicover$confusion
+pred_basicover$score(measure_ranger)
 
 
 #---- Get mDur and mFreq in winter and non-winter periods to assess whether intermittency is due to freezing ----
