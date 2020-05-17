c04351d2110ad84428470a85ac955de47a2c4b57
messamat <messamat@uw.edu>
2020-05-17 11:12:32 -0400

    Packages: Add installation for drake, edarf, and mlr3spatiotempcv

--- R/IRmapping_packages.R
+++ R/IRmapping_packages.R
@@ -6,6 +6,7 @@
 #library(pdp) #https://bgreenwell.github.io/pdp/articles/pdp.html for partial dependence — but very slow
 # install.packages("data.table", repos="https://Rdatatable.github.io/data.table")
 
+#devtools::install_github("ropensci/drake")
 suppressPackageStartupMessages(library(drake))
 suppressPackageStartupMessages(library(reprex))
 suppressPackageStartupMessages(library(tictoc))
@@ -20,7 +21,6 @@ suppressPackageStartupMessages(library(bigstatsr))
 suppressPackageStartupMessages(library(paradox))
 suppressPackageStartupMessages(library(ranger))
 suppressPackageStartupMessages(library(edarf)) 
-suppressPackageStartupMessages(library(xlsx))
 suppressPackageStartupMessages(library(ggpubr))
 suppressPackageStartupMessages(library(mlr3verse))
 suppressPackageStartupMessages(library(data.table))
@@ -30,8 +30,15 @@ suppressPackageStartupMessages(library(future.callr))
 suppressPackageStartupMessages(library(furrr))
 suppressPackageStartupMessages(library(quantreg))
 
+
+#remotes::install_github("https://github.com/mlr-org/mlr3spatiotempcv")
+suppressPackageStartupMessages(library(mlr3spatiotempcv))
+
 #remotes::install_github("mlr3learners/mlr3learners.partykit", INSTALL_opts = c("--no-multiarch"), force=T)
 suppressPackageStartupMessages(library(mlr3learners.partykit))
+
+#devtools::install_github("zmjones/edarf", subdir = "pkg")
+suppressPackageStartupMessages(library(edarf))
 
 # suppressPackageStartupMessages(library(bigstatsr))
 # suppressPackageStartupMessages(library(parallel))
