e0a3fb20589b81f07466792456da89b45a275dcf
messamat <messamat@uw.edu>
2020-05-24 17:31:13 -0400

    Start developing benchmarking curves in draft script

--- IntermittentAnalysis_MasterScript_reproduced.R
+++ IntermittentAnalysis_MasterScript_reproduced.R
@@ -3,97 +3,105 @@ source('R/IRmapping_packages.R')
 source('R/IRmapping_functions.R')
 source('R/IRmapping_plan.R')
 
+in_filestructure <- readd(filestructure)
+in_gaugep <- readd(gaugep)
 in_gaugestats <- readd(gaugestats_format)
-in_predvars <- readd(predvars)
 in_rftuned <- readd(rftuned)
-plot(readd(vimp_plot))
+in_predvars <- readd(predvars)
 
-gguncertainty <- function(in_rftuned, in_gaugestats, in_predvars) {
-  #Get average predictions for oversampled rows
-  gaugepred <-  in_rftuned$rf_outer$prediction() %>%
-    as.data.table %>%
-    .[, list(truth=first(truth), prob.1=mean(prob.1)), by=row_id] %>%
-    setorder(row_id)
-  
-  predattri <- cbind(in_gaugestats[!is.na(cly_pc_cav),], gaugepred) %>%
-    .[, `:=`(preduncert = prob.1-as.numeric(as.character(intermittent)),
-             yearskeptratio = totalYears_kept/totalYears)]
-  
-  #Plot numeric variables
-  predmelt_num <- predattri[, which(as.vector(unlist(lapply(predattri, is.numeric)))), with=F] %>%
-    cbind(predattri[, c('GRDC_NO', 'intermittent'), with=F]) %>%
-    melt(id.vars=c('GRDC_NO', 'intermittent', 'prob.1', 'preduncert'))
-  
-  
-  #Set variable labels
-  varlabels <- setkey(in_predvars[, .(varcode, varname)], varcode)[
-    levels(predmelt_num$variable)] %>%
-    .[is.na(varname), varname := varcode]
-  predmelt_num[, variable := factor(variable, labels = varlabels$varname)]
-  
-  varstoplot <- merge(in_predvars[, .(varcode, varname)], 
-                      data.table(varcode = 
-                                   c('totalYears_kept', 'yearskeptratio', 
-                                     'mDur', 'mFreq', 'station_river_distance', 
-                                     'UPLAND_SKM', 'ORD_STRA', 
-                                     'dis_m3_pyr', 'dor_pc_pva',
-                                     'cmi_ix_uyr','ari_ix_uav')), 
-                      all.x =F, all.y=T)
-  plotdt <- predmelt_num[variable %in% varstoplot$varcode | 
-                           variable %in% varstoplot$varname ,]
-  
-  uncertainty_numplot <- 
-    ggplot(plotdt, aes(x=value, y=preduncert, color=intermittent)) + 
-    geom_rect(xmin=-Inf, xmax=Inf, ymin=-0.5, ymax=0.5, 
-              fill='#d9d9d9', color='#d9d9d9') +
-    geom_point(alpha = 1/4) + 
-    geom_hline(yintercept=0, alpha=1/2) +
-    geom_smooth(method='gam', formula = y ~ s(x, k=3)) +
-    annotate("text", x = Inf-5, y = 0.5, angle = 90, 
-             label = "Pred:Int, Obs:Per",
-             color = '#1f78b4') +
-    annotate("text", x = Inf-5, y = -0.5, angle = 90, 
-             label = "Pred:Per, Obs:Int",
-             color = '#ff7f00') + 
-    scale_color_manual(values=c('#1f78b4', '#ff7f00'),
-                       name='Observed regime', 
-                       labels = c('Perennial', 'Intermittent')) + 
-    #scale_x_sqrt(expand=c(0,0)) +
-    coord_cartesian(clip='off') + 
-    facet_wrap(~variable, scales='free', labeller=label_value) + 
-    theme_classic() +
-    theme(legend.position = c(0.8, 0.1))
-  
-  
-  #Plot numeric variables
-  predmelt_cat <- predattri[, c('GRDC_NO', 'intermittent', 'preduncert',
-                                'ENDORHEIC', 'clz_cl_cmj'), with=F] %>%
-    melt(id.vars=c('GRDC_NO', 'intermittent', 'preduncert'))
-  
-  uncertainty_catplot <- 
-    ggplot(predmelt_cat, aes(x=as.factor(value), y=preduncert, 
-                             fill=intermittent, color=intermittent)) + 
-    geom_rect(xmin=-Inf, xmax=Inf, ymin=-0.5, ymax=0.5, 
-              fill='#d9d9d9', color='#d9d9d9', alpha=1/2) +
-    #geom_boxplot(alpha = 0.75) +
-    geom_violin(alpha=0.75, color=NA) +
-    geom_hline(yintercept=0, alpha=1/2) +
-    coord_cartesian(clip='off') + 
-    scale_fill_manual(values=c('#1f78b4', '#ff7f00'),
-                      name='Observed regime', 
-                      labels = c('Perennial', 'Intermittent')) + 
-    scale_color_manual(values=c('#175885', '#9e3f00'),
-                       name='Observed regime', 
-                       labels = c('Perennial', 'Intermittent')) + 
-    facet_wrap(~variable, scales='free', labeller=label_value) + 
-    theme_bw() +
-    theme(legend.position = c(0.8, 0.1))
-  
-  return(list(uncertainty_numplot, uncertainty_catplot))
-}
+loadd(rfbm_classif)
+in_rf = rfbm_classif$bm_classif$clone()$filter(learner_ids = "oversample.classif.ranger.tuned")
+in_task = rfbm_classif$bm_tasks$task_classif
+in_measure = rfbm_classif$measure_classif
+featimpfilt = 0.01
+insamp_nfolds =  2
+insamp_nevals = 1
+outsamp_nrep = 2
+outsamp_nfolds =  10
+imp = TRUE
+perf = TRUE
+pvalue = TRUE
+pvalue_permutn = 10
+
+
+rfresamp <-  in_rf$resample_result(
+  uhash=unique(as.data.table(in_rf)$uhash))
+
+bmcheck_classif <- readd(bm_checked_rfbm_classif.bm_classif_rfbm_classif.measure_classif)
+bmcheck_regr <- readd(bm_checked_rfbm_regr.bm_regr_rfbm_regr.meassure_regr)
+loadd(rfbm_classif)
+loadd(rfbm_regr)
+loadd(rfeval_featsel)
 
 
 
+bmres <- rfbm_classif$bm_classif
+check <- lapply(1:bmres$n_resample_results, function(i) {
+  print(i)
+  rsmp_preds <- bmres$resample_result(rsmp_i)$prediction()
+
+  baccthresh <- ldply(seq(0,1,0.01), function(threshold_class) {
+    print(threshold_class)
+    cbind(
+      rsmp_bacc(bmres, rsmp_i=i, threshold_class=threshold_class),
+      threshold_misclass(i=threshold_class, in_preds=rsmp_preds)
+    ) %>%
+      .[, i:=NULL]
+  }) %>%
+    setDT
+  return(baccthresh)
+})
+
+gout <- ggplot(melt(threshold_confu_dt, id.vars='i'),
+               aes(x=i, y=value, color=variable, linetype=variable)) +
+  geom_line(size=1.2) +
+  geom_vline(xintercept=balanced_thresh$i, alpha=1/2) +
+  geom_hline(yintercept=balanced_thresh$spec, alpha=1/2) +
+  annotate('text', x=(balanced_thresh$i), y=0.4,
+           label=balanced_thresh$i, angle=-90) +
+  annotate('text', x=0.9, y=(balanced_thresh$spec),
+           label=round(balanced_thresh$sens,2)) +
+  scale_x_continuous(expand=c(0,0), name='Threshold') +
+  scale_y_continuous(expand=c(0,0), name='Value') +
+  scale_color_brewer(palette='Dark2',  #colorblind friendly
+                     labels=c('Misclassification rate',
+                              'Sensitivity (true positives)',
+                              'Specificity (true negatives)')) +
+  theme_bw()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+################### KEEP TO ANALYZE SPATIAL RESAMPLING OUTPUT ###################
+check <- nestedresamp_bmrout_classif$resample_result(2)
+coordfolds <- cbind(nestedresamp_bmrout_classif$tasks$task[[1]]$coordinates(), check$resampling$instance)
+
+library("rnaturalearth")
+library("rnaturalearthdata")
+library(rgeos)
+
+world <- rnaturalearth::ne_countries(scale = "medium", returnclass = "sf")
+
+ggplot() +
+  geom_sf(data = world)+
+  geom_sf(data=sf::st_as_sf(x =coordfolds, coords = c("X", "Y"),
+                            crs = "+proj=longlat +datum=WGS84"),
+          aes(color=as.factor(fold)))
 
 
 
@@ -103,7 +111,7 @@ gguncertainty <- function(in_rftuned, in_gaugestats, in_predvars) {
 window <- 3
 mintemplist<- dcast(
   melt(
-    reaches_with_points[, c('GRDC_NO', grep('^tmp.*_c[0-9]{2}', colnames(reaches_with_points), value = T)), with=F], 
+    reaches_with_points[, c('GRDC_NO', grep('^tmp.*_c[0-9]{2}', colnames(reaches_with_points), value = T)), with=F],
     id.var = 'GRDC_NO'),
   variable~GRDC_NO, value.var='value')[,-1] %>% #Pre-format temperature
   frollapply(n=window, FUN=mean, align="center") %>% #Compute 3-month rolling mean
